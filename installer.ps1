47ac4e7fd790712398aaa42bfdeff84634217668
CoriumCake <45147371+CoriumCake@users.noreply.github.com>
2023-12-01 02:55:46 +0700

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -17,13 +17,13 @@ function Install-VSCode {
 }
 
 function Install-7zip {
-    if (Test-Path -Path .\7zip) {
-        Write-Host "7zip installed"
-    } elseif (!(Test-Path -Path .\7zip.zip)) {
+    if (Test-Path -Path ".\7zip") {
+        Write-Host "7zip installed`n"
+    } elseif (!(Test-Path -Path ".\7zip.zip")) {
         Write-Host "Pulling 7zip"
         $ProgressPreference = 'SilentlyContinue'
-        Invoke-WebRequest "https://github.com/CoriumCake/vscode-cpp-quickstart/raw/main/7-Zip.zip" -OutFile (New-Item -Path "7zip.zip" -Force)
-        Expand-Archive -Force 7zip.zip 
+        Invoke-WebRequest "https://github.com/CoriumCake/vscode-cpp-quickstart/raw/main/7-Zip.zip" -OutFile (New-Item -Path ".\7zip.zip" -Force)
+        Expand-Archive -Force .\7zip.zip 
         Write-Host "7zip installed`n"
 
         # Get the full path of the 7-Zip directory
@@ -34,26 +34,28 @@ function Install-7zip {
 
         if (-not $pathExists) {
             # Add the 7-Zip directory to the system PATH
-            $newPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine) + ";$7zipPath"
-            [System.Environment]::SetEnvironmentVariable("Path", $newPath, [System.EnvironmentVariableTarget]::Machine)
-            Write-Host "7zip env added`n"
+            [System.Environment]::SetEnvironmentVariable("Path", "$7zipPath;$env:Path", [System.EnvironmentVariableTarget]::Machine)
+            Write-Host "7zip directory added to the system PATH`n"
         } else {
-            Write-Host "7zip env already exists`n"
+            Write-Host "7zip directory is already in the system PATH`n"
         }
     }
 }
 
 function Install-Mingw {
-    if (Test-Path -Path mingw) {
+    if (Test-Path -Path ".\mingw64") {
         Write-Host "Mingw installed"
-    } elseif (!(Test-Path -Path mingw.7z)) {
-        Write-Host "Pulling mingw`n"
+    } elseif (!(Test-Path -Path ".\mingw.7z")) {
+        Write-Host "Downloading mingw.7z`n"
         $ProgressPreference = 'SilentlyContinue'
-        Invoke-WebRequest "https://github.com/niXman/mingw-builds-binaries/releases/download/13.2.0-rt_v11-rev1/x86_64-13.2.0-release-mcf-seh-ucrt-rt_v11-rev1.7z" -OutFile (New-Item -Path "mingw.7z" -Force)
+        Invoke-WebRequest "https://github.com/niXman/mingw-builds-binaries/releases/download/13.2.0-rt_v11-rev1/x86_64-13.2.0-release-mcf-seh-ucrt-rt_v11-rev1.7z" -OutFile (New-Item -Path ".\mingw.7z" -Force)
+        Write-Host "mingw.7z downloaded`n"
+    }
 
-        & "$env:path\7z.exe" x -o"." "mingw.7z" -r -y
-
-        Write-Host ""
+    if (Test-Path -Path ".\mingw.7z") {
+        Write-Host "Extracting mingw.7z`n"
+        # Use 7zip to extract the contents of the 7z file
+        & ".\7zip\7-Zip\7z.exe" x -o"." ".\mingw.7z" -r -y
         Write-Host "Mingw installed`n"
 
         # Get the full path of the Mingw directory
