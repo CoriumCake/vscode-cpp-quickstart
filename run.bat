43fa018a14687b0e62e64c05ad8051703e232d9e
CoriumCake <45147371+CoriumCake@users.noreply.github.com>
2023-12-01 03:21:25 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 curl -L -o installer.ps1 https://raw.githubusercontent.com/CoriumCake/vscode-cpp-quickstart/main/installer.ps1
-PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0installer.ps1""' -Verb RunAs}"
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0installer.ps1"
