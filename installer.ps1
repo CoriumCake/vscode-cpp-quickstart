6c10d5e0ac8a686d3516ff5e0a426e9c763763d7
CoriumCake <45147371+CoriumCake@users.noreply.github.com>
2023-12-01 03:47:03 +0700

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -18,13 +18,14 @@ function Install-VSCode {
 
 function Install-7zip {
     if (Test-Path -Path ".\7zip") {
+        Write-Host ""
         Write-Host "7zip installed`n"
     } elseif (!(Test-Path -Path ".\7zip.zip")) {
-        Write-Host ""
         Write-Host "Pulling 7zip"
         $ProgressPreference = 'SilentlyContinue'
         Invoke-WebRequest "https://github.com/CoriumCake/vscode-cpp-quickstart/raw/main/7-Zip.zip" -OutFile (New-Item -Path ".\7zip.zip" -Force)
-        Expand-Archive -Force .\7zip.zip 
+        Expand-Archive -Force .\7zip.zip
+        Write-Host ""
         Write-Host "7zip installed`n"
 
         $7zipPath = (Get-Item -Path ".\7zip\7-Zip").FullName
